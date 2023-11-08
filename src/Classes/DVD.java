f53d941cd02b77dcda56e0f817d841483f3abd0d
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-11-08 22:11:48 +0300

    save 08/11/23

--- src/Classes/DVD.java
+++ src/Classes/DVD.java
@@ -1,6 +1,13 @@
 package Classes;
 
+import Interfaces.ILibraryItem;
+
 //3) Создайте класс DVD, который реализует интерфейс LibraryItem.
 // В классе DVD переопределите метод getDescription(), чтобы он возвращал строку вида "DVD: [название DVD]".
-public class DVD {
+public class DVD implements ILibraryItem {
+    @Override
+    public void getDescription() {
+        System.out.println("DVD: [Сказки Пушкина]");
+
+    }
 }
--- src/Interfaces/ILibraryItem.java
+++ src/Interfaces/ILibraryItem.java
@@ -3,5 +3,6 @@ package Interfaces;
 //2) Создайте интерфейс LibraryItem, который будет представлять элемент библиотеки.
 // У этого интерфейса должен быть метод getDescription(), который возвращает описание элемента библиотеки.
 public interface ILibraryItem { //элемент библиотеки
-    public void Description(); //место
+    public void getDescription();//получить описание
+
 }
