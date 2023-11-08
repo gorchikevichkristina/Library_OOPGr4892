32b442c71ee1f437e7f1874b804dca8fa9f0780b
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-11-08 22:34:57 +0300

    save 08/11/23

--- src/Classes/Library.java
+++
@@ -1,4 +0,0 @@
-package Classes;
-//6) Создайте класс Library, который будет представлять библиотеку. Этот класс должен содержать список элементов LibraryItem.
-public class Library {
-}
---
+++ src/Library.java
@@ -0,0 +1,24 @@
+import Interfaces.ILibraryItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//6) Создайте класс Library, который будет представлять библиотеку.
+// Этот класс должен содержать список элементов LibraryItem.
+public class Library {
+    public static void main(String[] args) {
+        Map<String, List<String>> Library = new HashMap<>();
+
+        addItem(Library, "Сказки", "Пушкин А.С.");
+        addItem(Library, "Зов Ктулху", "Лавкрафт Г.Ф.");
+        addItem(Library, "ОНО", "Кинг С.");
+
+    }
+    // Метод для добавления контакта и номера в телефонную книгу
+    public static void addItem(Map<String, List<String>> Library, String title, String author) {
+        Library.computeIfAbsent(title, k -> new ArrayList<>()).add(author);
+    }
+
+}
